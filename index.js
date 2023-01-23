c9b3cda24772b38a87fc2bb068003052b1776ca9
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-01-23 05:03:14 +0000

    Final Code

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 // .ENV
 require("dotenv").config();
-const express = require('express')
-const app = express()
-const port = 8080
-var XMLHttpRequest = require('xhr2');
-app.get('/', (req, res) => {
-  res.send('The Status will update after every 30 second , no instant update is visible, so just listen the song')
-})
+//importing all modules
+const express = require("express");
+const app = express();
+const port = 8080;
+const fs = require("fs");
+const revolt = require("revolt.js");
+const XMLHttpRequest = require("xhr2");
+const request = require("request");
+const fs = require("fs");
+
+//starting web host
+app.get("/", (req, res) => {
+  res.send(
+    "The Status will update after every 30 second , no instant update is visible, so just listen the song"
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Server online!`)
-})
+  console.log(`Server online!`);
+});
 
-// Code
-const fs = require('fs');
-const revolt = require('revolt.js');
+// client created
 const client = new revolt.Client();
-thetoken = process.env.user_token;
+thetoken = process.env.user_token; //your user_token
+//fetching song and artist name and updatin status
 function spotify(username, api_key) {
-  var request = require("request");
-  const fs = require("fs"); //If you get any sort of error when first running, try type 'npm install fs' into your terminal
   var headers = {
     Accept: "application/json",
   };
@@ -39,22 +45,16 @@ function spotify(username, api_key) {
         const lastSong = student.recenttracks.track?.[0];
 
         if (!lastSong || !lastSong["@attr"]?.nowplaying) {
-          // console.log("Not playing anything");
           return "Not playing anything";
-        }
-        else {
-          const artist = `${lastSong.artist["#text"]}`;
-          const song = `${lastSong.name}`;
-          console.log(`playing ${song} by ${artist}`);
-          // const text = `🎵playing ${song} by ${artist}`;
-          // client.users.edit({
-          //   status: {
-          //     text: text
-          //   }
-          // });
-          thestatus = `🎵playing ${song} by ${artist}`
+        } else {
+          const artist = `${lastSong.artist["#text"]}`; //artist name
+          const song = `${lastSong.name}`; //song name
+
+          console.log(`playing ${song} by ${artist}`); // display on console
+          thestatus = `🎵playing ${song} by ${artist}`; //display on status
+          //updating status from x-session-token
           function request(lareq, laurl, lathing, after) {
-            laurl = "https://api.revolt.chat" + laurl
+            laurl = "https://api.revolt.chat" + laurl;
             var statusreq = new XMLHttpRequest();
             statusreq.open(lareq, laurl, true);
             if (thetoken !== undefined) {
@@ -63,11 +63,11 @@ function spotify(username, api_key) {
             statusreq.setRequestHeader("Accept", "*/*");
             statusreq.setRequestHeader("Content-Type", "application/json");
 
-            statusreq.onreadystatechange = function() {
+            statusreq.onreadystatechange = function () {
               if (statusreq.readyState === 4) {
                 status = statusreq.status;
                 resp = statusreq.responseText;
-                console.log(lareq + ": " + laurl + ", got " + statusreq.status);
+                console.log(lareq + ": " + laurl + ", got " + statusreq.status); //debuging status update on console
                 if (typeof after == "function") {
                   after(status, resp);
                 }
@@ -75,23 +75,25 @@ function spotify(username, api_key) {
             };
             if (lathing == null) {
               statusreq.send();
-            }
-            else {
+            } else {
               statusreq.send(lathing);
             }
           }
 
-          request("PATCH", "/users/@me", '{"status":{"text":"' + thestatus + '"}}');
+          request(
+            "PATCH",
+            "/users/@me",
+            '{"status":{"text":"' + thestatus + '"}}'
+          );
           return `playing ${song} by ${artist}`;
         }
-
       }
     }
     request(options, callback);
   }, 30000);
 }
 
-const username = `${process.env.spotify_username}`;
-const api_key = `${process.env.api_key}`;
+const username = `${process.env.spotify_username}`; //spotify username
+const api_key = `${process.env.api_key}`; //last.fm api key , refer readme.md to know how to get it
 
 spotify(username, api_key);
